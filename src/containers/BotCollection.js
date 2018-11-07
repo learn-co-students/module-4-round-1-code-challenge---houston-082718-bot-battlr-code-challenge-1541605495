6764f2c4eea659fb4432b94368f0ddff02eb35a4
sparkbold-git <sparkboldstudio@gmail.com>
2018-11-07 10:43:14 -0600

    Reach checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,17 +4,24 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  render() {
+    console.log(this.props);
+    const { botsList } = this.props;
+    return (
+      <div className="ui four column grid">
+        <div className="row">
+          {botsList.map(bot => (
+            <BotCard
+              key={bot.id}
+              bot={bot}
+              onClickBot={this.props.onClickBot}
+            />
+          ))}
+          Collection of all bots
+        </div>
+      </div>
+    );
   }
-
-};
+}
 
 export default BotCollection;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,51 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
+const api = "https://bot-battler-api.herokuapp.com/api/v1/bots";
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    botsList: [],
+    yourArmyList: []
+  };
+  componentDidMount() {
+    fetch(api)
+      .then(resp => resp.json())
+      .then(
+        data => this.setState({ botsList: data }),
+        console.log("Success fetch")
+      );
+  }
 
+  onClickBot = bot => {
+    this.setState({ yourArmyList: [...this.state.yourArmyList, bot] }, () =>
+      console.log("armyList", this.state.yourArmyList)
+    );
+  };
+
+  onClickArmyBot = bot => {
+    const newArmyList = [...this.state.yourArmyList].filter(
+      armyBot => armyBot.id !== bot.id
+    );
+    this.setState({ yourArmyList: newArmyList }, () =>
+      console.log("newList", this.state.yourArmyList)
+    );
+  };
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy
+          yourArmyList={this.state.yourArmyList}
+          onClickBot={this.onClickArmyBot}
+        />
+        <BotCollection
+          botsList={this.state.botsList}
+          onClickBot={this.onClickBot}
+        />
       </div>
     );
   }
-
 }
 
 export default BotsPage;
