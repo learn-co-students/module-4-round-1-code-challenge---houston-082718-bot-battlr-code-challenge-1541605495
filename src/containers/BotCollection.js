0cb34f68a69824ea230b70e98706015c3d5e95d2
Chelsea Crowson <chelsea.crowson@me.com>
2018-11-07 10:18:34 -0600

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,18 +2,19 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
+	//your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
-  }
+	render() {
+		return (
+			<div className="ui four column grid">
+				<div className="row">
+					{this.props.allBots.map((bot) => {
+						return <BotCard key={bot.id} bot={bot} addToArmyHandler={this.props.addToArmyHandler} />
+					})}
+				</div>
+			</div>
+		);
+	}
 
 };
 
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,46 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    allBots: [],
+    botArmy: []
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          allBots: data
+        })
+      })
+  }
+
+  addToArmyHandler = (bot) => {
+    !this.state.botArmy.includes(bot) ?
+      this.setState({
+        botArmy: [...this.state.botArmy, bot]
+      }) :
+      null
+  }
+
+  removeFromArmyHandler = (selectedBot) => {
+    const newBots = this.state.botArmy.filter((bot) => {
+      return bot !== selectedBot
+    })
+    this.setState({
+      botArmy: newBots
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy botArmy={this.state.botArmy} removeFromArmyHandler={this.removeFromArmyHandler} />
+        <BotCollection allBots={this.state.allBots} addToArmyHandler={this.addToArmyHandler} />
       </div>
     );
   }
