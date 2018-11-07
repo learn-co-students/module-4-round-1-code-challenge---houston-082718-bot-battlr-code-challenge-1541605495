d8f1ce02587a8fec005a8bce9f2a2cbf94181af6
Nick Luong <nckluong@gmail.com>
2018-11-07 10:29:23 -0600

    i played myself

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,17 +4,24 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  render() {
+    return (
+      <div className="ui four column grid">
+        <div className="row">
+          <h1>Collection of all bots</h1>
+          {this.props.bots.map(bot => {
+            return (
+              <BotCard
+                key={bot.id}
+                bot={bot}
+                handleClick={() => this.props.handleClick(bot)}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
   }
-
-};
+}
 
 export default BotCollection;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,42 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    bots: [],
+    army: []
+  };
+
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(r => r.json())
+      .then(r => this.setState({ bots: r }));
+  }
+
+  handleClick = bot => {
+    if (this.state.army.includes(bot)) {
+      const newArr = this.state.army.filter(armyBot => {
+        return armyBot.id !== bot.id;
+      });
+      this.setState({ army: newArr });
+    } else {
+      this.setState({
+        army: [...this.state.army, bot]
+      });
+    }
+  };
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy army={this.state.army} handleClick={this.handleClick} />
+        <BotCollection bots={this.state.bots} handleClick={this.handleClick} />
       </div>
     );
   }
-
 }
 
 export default BotsPage;
