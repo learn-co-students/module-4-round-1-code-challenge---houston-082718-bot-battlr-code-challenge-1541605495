310f33dec97352d2bfda109489e22dd71f91f5ad
sparkbold-git <sparkboldstudio@gmail.com>
2018-11-07 11:29:24 -0600

    Done all deliverables

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import BotCollection from "./BotCollection";
 import YourBotArmy from "./YourBotArmy";
+import BotSpecs from "../components/BotSpecs";
+import Filter from "../components/Filter";
 
 const api = "https://bot-battler-api.herokuapp.com/api/v1/bots";
 class BotsPage extends React.Component {
   //start here with your code for step one
   state = {
     botsList: [],
-    yourArmyList: []
+    yourArmyList: [],
+    showBotSpecs: false,
+    selectedBot: {}
   };
   componentDidMount() {
     fetch(api)
@@ -18,13 +22,15 @@ class BotsPage extends React.Component {
       );
   }
 
-  onClickBot = bot => {
-    this.setState({ yourArmyList: [...this.state.yourArmyList, bot] }, () =>
-      console.log("armyList", this.state.yourArmyList)
-    );
+  onClickAddBot = bot => {
+    !this.state.yourArmyList.includes(bot) &&
+      this.setState({ yourArmyList: [...this.state.yourArmyList, bot] }, () =>
+        console.log("armyList", this.state.yourArmyList)
+      );
   };
 
-  onClickArmyBot = bot => {
+  onClickRemoveBot = bot => {
+    console.log("AddArmyBotFired", bot);
     const newArmyList = [...this.state.yourArmyList].filter(
       armyBot => armyBot.id !== bot.id
     );
@@ -32,17 +38,40 @@ class BotsPage extends React.Component {
       console.log("newList", this.state.yourArmyList)
     );
   };
+
+  onClickBotShowSpecs = bot => {
+    this.setState({ ...this.state, showBotSpecs: true, selectedBot: bot }, () =>
+      console.log("selected bot", this.state.selectedBot)
+    );
+  };
+
+  onClickGoBack = () => {
+    console.log("GoBackFired");
+    this.setState({ ...this.state, showBotSpecs: false }, () =>
+      console.log("GoBack", this.state.showBotSpecs)
+    );
+  };
+
   render() {
     return (
       <div>
+        <Filter />
         <YourBotArmy
           yourArmyList={this.state.yourArmyList}
-          onClickBot={this.onClickArmyBot}
+          onClickBot={this.onClickRemoveBot}
         />
-        <BotCollection
-          botsList={this.state.botsList}
-          onClickBot={this.onClickBot}
-        />
+        {this.state.showBotSpecs ? (
+          <BotSpecs
+            bot={this.state.selectedBot}
+            onClickGoBack={this.onClickGoBack}
+            onClickBot={this.onClickAddBot}
+          />
+        ) : (
+          <BotCollection
+            botsList={this.state.botsList}
+            onClickBot={this.onClickBotShowSpecs}
+          />
+        )}
       </div>
     );
   }
