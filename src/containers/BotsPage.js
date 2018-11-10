eb494df720f6dcc255f3dea5f51000a64950d24c
V10LET <violet.wmoon@gmail.com>
2018-11-09 19:22:15 -0600

    added dropdown feature: 40min

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,12 +2,21 @@ import React from "react"
 import BotCollection from "./BotCollection"
 import YourBotArmy from "./YourBotArmy"
 import BotSpecs from "../components/BotSpecs"
+import { Dropdown } from "semantic-ui-react"
+
+const options = [
+        {key: 'All', value: 'All', text: 'All'},
+        {key: 'Defender', value: 'Defender', text: 'Defender'},
+        {key: 'Assault', value: 'Assault', text: 'Assault'},
+        {key: 'Support', value: 'Support', text: 'Support'}
+    ]
 
 class BotsPage extends React.Component {
     state = {
         bots: [],
         army: [],
-        clicked: {boolean: false, bot: null}
+        clicked: {boolean: false, bot: null},
+        option: ''
     }
 
     componentDidMount() {
@@ -32,20 +41,27 @@ class BotsPage extends React.Component {
         army.splice(index, 1)
         const newArmy = army
         this.setState({ army: newArmy })
-
     }
 
     handleBack = () => {
         this.setState({ clicked: {boolean: !this.state.clicked.boolean, bot: null} })
     }
 
+    handleDropDown = (event) => {
+        const selected = event.target.innerText
+        selected === 'All' ? this.setState({ option: '' }) : this.setState({ option: selected })
+    }
+
     render() {
+        const bots = this.state.bots.filter(bot => bot.bot_class.includes(this.state.option))
+
         return (
-            <div>
+            <div className='container'>
+                <Dropdown placeholder='Select Bot Class' selection options={options} onChange={this.handleDropDown}/>
                 <YourBotArmy army={this.state.army} handleRemove={this.handleRemove} />
                 {this.state.clicked.boolean
                     ? <BotSpecs bot={this.state.clicked.bot} handleEnlist={this.handleEnlist} handleBack={this.handleBack}/>
-                : <BotCollection bots={this.state.bots} handleProfile={this.handleProfile}/>}
+                : <BotCollection bots={bots} handleProfile={this.handleProfile}/>}
             </div>
         )
     }
